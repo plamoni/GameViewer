8c32aa45288743a29c52436952de1f7c1c397368
Pete Lamonica <plamoni@gmail.com>
2010-09-24 14:25:55 -0500

    removed some files that were left over from before the appengine conversion

--- helperfunctions.js
+++
@@ -1,9 +0,0 @@
-function secondsToTime(seconds) {
-	if(seconds < 3600) return Math.floor(seconds / 60) + ":" + ((seconds % 60 < 10) ? "0" : "") + Math.floor(seconds % 60);
-	return Math.floor(seconds / 3600) + ":" + ((seconds % 60 < 10) ? "0" : "") + Math.floor((seconds / 60) % 60) + ":" + ((seconds % 60 < 10) ? "0" : "") + Math.floor(seconds % 60);
-}
-
-function playerStateStr(stateId) {
-	var states = ["unstarted", "ended", "playing", "paused", "buffering", "unknown", "cued"];
-	return states[stateId + 1];
-}
